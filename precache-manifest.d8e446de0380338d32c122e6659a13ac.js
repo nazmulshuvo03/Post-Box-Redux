42a8c6459d8188e46d2e23b7460b31f72ec74135
Nazmul Alom <nazmulshuvo03@gmail.com>
2019-03-16 16:08:26 +0600

    Updates

---
+++ precache-manifest.d8e446de0380338d32c122e6659a13ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7c6f18924d89da356be",
+    "url": "/Post-Box-Redux/static/js/runtime~main.b7c6f189.js"
+  },
+  {
+    "revision": "3ec00ce1fa57d5af7586",
+    "url": "/Post-Box-Redux/static/js/main.3ec00ce1.chunk.js"
+  },
+  {
+    "revision": "6eb71498c7d5d695f0d7",
+    "url": "/Post-Box-Redux/static/js/1.6eb71498.chunk.js"
+  },
+  {
+    "revision": "3ec00ce1fa57d5af7586",
+    "url": "/Post-Box-Redux/static/css/main.7696d0d0.chunk.css"
+  },
+  {
+    "revision": "743389819a006250c7b667b7e29cc1ea",
+    "url": "/Post-Box-Redux/index.html"
+  }
+];
